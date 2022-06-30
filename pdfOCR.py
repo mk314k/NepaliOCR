bc9a5b86158c1d6e62a0b28e59b390e1b4197be9
Kartikesh Mishra <mk314k@mit.edu>
2022-06-30 11:24:13 +0545

    intial work on model and main function

--- pdfOCR.py
+++ pdfOCR.py
@@ -1,8 +1,28 @@
 import numpy as np
 import cv2
 import os
+import ghostscript
+import locale
+#from pathlib import Path
+#path=Path('.').absolute().__str__()
+#print(fpath)
+filepath = os.getcwd()
+#print(filepath)
 
-print(os.getcwd())
+def pdf2jpeg(pdf_input_path, jpeg_output_path):
+    args = ["pef2jpeg", # actual value doesn't matter
+            "-dNOPAUSE",
+            "-sDEVICE=jpeg",
+            "-r144",
+            "-sOutputFile=" + jpeg_output_path,
+            pdf_input_path]
+
+    encoding = locale.getpreferredencoding()
+    args = [a.encode(encoding) for a in args]
+
+    ghostscript.Ghostscript(*args)
+
+#pdf2jpeg(filepath+"/NepaliOCR/NepaliOCR/bhanutest.pdf",filepath+"/out%02d.jpg")
 
 def pdfToImages(filename:str):
     print(os.environ)
