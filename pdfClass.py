668603b9ebac864af6e75cd68349c216531618bc
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-08 15:36:44 +0545

    Update pdfClass.py

--- pdfClass.py
+++ pdfClass.py
@@ -8,7 +8,7 @@ import locale
 
 class PDFReader():
     def __init__(self,fileName:str) -> None:
-        self.fileName=fileName
+        self.__fileName=fileName
     
     def toImage(self, outExt='jpg'):
         if outExt=='jpg':
@@ -19,8 +19,8 @@ class PDFReader():
             "-dNOPAUSE",
             "-sDEVICE=jpeg",
             "-r144",
-            "-sOutputFile=" + self.fileName[:-4]+'%02d.jpg',
-            self.fileName]
+            "-sOutputFile=" + self.__fileName[:-4]+'%02d.jpg',
+            self.__fileName]
 
         encoding = locale.getpreferredencoding()
         args = [a.encode(encoding) for a in args]
@@ -29,10 +29,25 @@ class PDFReader():
 
 
 class PDFWriter():
-    def __init__(self) -> None:
-        
-        pass
+    def __init__(self,filename) -> None:
+        self.__pdf =FPDF(orientation='P',unit='mm', format='A4')
+        self.__filename = filename
+    
+    def addText(self,textData):
+        self.__pdf.set_font_size(textData['size'])
+        self.__pdf.set_text_color(*textData['colr'])
+        self.__pdf.cell(textData['width'],textData['height'],textData['text'])
 
+    def addImage(self,imageData):
+        img=Image.fromarray(imageData['img'])
+        self.__pdf.image(img,x=imageData['x'],y=imageData['y'],w=imageData['w'],h=imageData['h'])
+
+    def addTable(self,tableData):
+
+        pass
+    
+    def close(self):
+        self.__pdf.output(self.__filename)
 
 def superPDF(fileName:str,mode='r')->object:
     if mode =='r':
