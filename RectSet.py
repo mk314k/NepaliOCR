278be228c762e1d44738929b7c11ffc82bb8e4e6
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-08 15:35:06 +0545

    Update RectSet.py

--- RectSet.py
+++ RectSet.py
@@ -1,56 +1,46 @@
 from Rect import Rect
 import numpy as np
-
-BLOCK_SIZE =50
-
-class DynamicNpy():
-    def __init__(self,objectType=Rect) -> None:
-        self.array = np.zeros(10).astype(objectType)
-        self.nullIndex = []
-        self.lastValueIndex =0
-    def __getitem__(self,index):
-        return self.array[index]
-    def __setitem__(self,index,value):
-        if index >= self.array.size:
-            arr = np.zeros(2*index)
-            arr[0:self.array.size] = self.array
-            self.array = arr
-        self.array[index]=value
-        if index>=self.lastValueIndex:
-            self.lastValueIndex=index+1
-    def append(self,value):
-        index = self.lastValueIndex
-        if self.nullIndex:
-            index = self.nullIndex.pop()
-        self.__setitem__(index,value)
-        pass
-    def remove(self,index):
-        self.array[index]=0
-        self.nullIndex.append(index)
-        pass
+from DynamicNP import DynamicNpy
 
 class RectSet():
-    def __init__(self,dimension:int|tuple) -> None:
+    def __init__(self,dimension:int|tuple,blockSize=200,blockMode=True) -> None:
+        self.__blocksize = blockSize
+        self.__blockMode = blockMode
+        self.__image = DynamicNpy(Rect)
         if isinstance(dimension,int):
             dimension =(dimension,dimension)
-        dimension = (dimension[0]//BLOCK_SIZE+1,dimension[1]//BLOCK_SIZE+1)
-        self.__image = np.zeros(dimension).astype(DynamicNpy)
+        self.__dimension = dimension
+        if blockMode:
+            dimension = (dimension[0]//blockSize+1,dimension[1]//blockSize+1)
+            self.__blocks = np.array([None]*dimension[0]*dimension[1]).reshape(dimension).astype(DynamicNpy)
         
-    def __eq__(self, __o: object) -> bool:
-        if self.__image.shape != __o.__image.shape : return False
-        return True
+    def __eq__(self, o: object) -> bool:
+        return self.__image.__eq__(o.__image)
 
     def addRect (self,rect:Rect):
-        blocks = rect.getblocks(BLOCK_SIZE)
-        for block in blocks:
-            if self.__image[block]==0:
-                self.__image[block]=set([rect])
+        indice = range(0,self.__image.size())
+        if self.__blockMode:
+            blocks = rect.getblocks()
+            for block in blocks:
+                indice = indice + self.__blocks[block] #TODO
+        
+        for index in indice:
+            if self.__image[index]==None:
+                self.__image[index]=DynamicNpy()
             else:
-                for i, existingRect in enumerate(self.__image[block]):
-                    if existingRect.isOverlapping(rect):
-                        rect =rect + existingRect
-                        self.__image[block].remove(i)
+                for i, existingRect in enumerate(self.__image[index]):
+                    if isinstance(existingRect,Rect) and existingRect.isOverlapping(rect):
+                        rect.update(existingRect)
+                        self.__image[block].removeIndex(i)
+                        print("overlapping",rect,existingRect,self.__image)
             self.__image[block].append(rect)
+        print(rect,self.__image)
+    def __iter__(self):
+        yield from self.__image
+
+    def __repr__(self) -> str:
+        return self.__image.__repr__()
+        pass
 
 
 
