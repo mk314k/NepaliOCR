d12e8d7dbba0caa308f540bb1a1d10197f47e343
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-13 17:22:19 +0545

    Update DynamicNP.py

--- DynamicNP.py
+++ DynamicNP.py
@@ -1,41 +1,75 @@
 import numpy as np
+from interfaces import classObj
 
 class DynamicNpy():
-    def __init__(self,objectType,initlen=2) -> None:
-        self.array = np.array([None]*initlen).astype(objectType)
-        self.nullindex = []
-        self.objType = objectType
-        self.lastValueIndex =0
-    def __getitem__(self,index):
-        return self.array[index]
-    def __setitem(self,index,value):
-        if index >= self.array.size:
-            arr = np.array([None]*2*index).astype(self.objType)
-            arr[0:self.array.size] = self.array
-            self.array = arr
-        self.array[index]=value
-        if index>=self.lastValueIndex:
-            self.lastValueIndex=index+1
+    
+    def __init__(self,objectType:classObj,initlen=2,allowedObjType=None) -> None:
+        """AI is creating summary for __init__
+
+        Args:
+            objectType ([type]): [description]
+            initlen (int, optional): [description]. Defaults to 2.
+        """
+        self.__array = np.array([None]*initlen).astype(objectType)
+        self.__nullIndex = []
+        self.__objType = objectType
+        self.__lastValueIndex =-1
+        self.__allowedObjType = allowedObjType
+
+    def __getitem__(self,index:int):
+        """AI is creating summary for __getitem__
+
+        Args:
+            index ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.__array[index]
+
+    def __setitem(self,index:int,value:classObj):
+        # if not isinstance(value,self.__objType):
+        #     if isinstance(value,self.__allowedObjType):
+        #         value=self.__objType(value)
+        #     else:
+        #         raise Exception("This array is not defined to store given value object type")
+
+        if index >= self.__array.size:
+            arr = np.array([None]*2*index).astype(self.__objType)
+            arr[0:self.__array.size] = self.__array
+            self.__array = arr
+
+        self.__array[index]=value
+        if index>self.__lastValueIndex:
+            self.__lastValueIndex=index
         return index
-    def __setitem__(self,index,value):
-        _=self.__setitem(index,value)
+
+    # def __setitem__(self,index,value):
+    #     _=self.__setitem(index,value)
+
     def append(self,value):
-        index = self.lastValueIndex
-        if self.nullindex:
-            index = self.nullindex.pop()
+        index = self.__lastValueIndex + 1
+        if self.__nullIndex:
+            index = self.__nullIndex.pop()
         return self.__setitem(index,value)
-        pass
+
     def removeIndex(self,index):
-        self.array[index]=None
-        self.nullindex.append(index)
-        pass
+        if (index<0 or index>=self.size()):
+            raise Exception(f"Index out of bound. Array is size of {self.size()} while {index} is called")
+        self.__array[index]=None
+        self.__nullIndex.append(index)
+        #TODO
+        # if len(self.__nullIndex)>=self.size()//2:
+        #     arr = 
+
     def size(self):
-        return self.array.size
+        return self.__array.size
+
     def __iter__(self):
-        for obj in self.array:
-            if isinstance(obj,self.objType):
+        for obj in self.__array:
+            if isinstance(obj,self.__objType):
                 yield obj
 
     def __repr__(self) -> str:
-        return self.array.__repr__()
+        return self.__array.__repr__()
         pass
