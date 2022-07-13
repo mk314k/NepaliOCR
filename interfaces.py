f3970e022496bb105d753e21eb3622285513a73e
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-13 17:21:37 +0545

    Update interfaces.py

--- interfaces.py
+++ interfaces.py
@@ -5,18 +5,14 @@ class Rectangle():
     __x1:int
     __x2:int
     __y1:int
-    __y2:int
-    # def lowerLeftPoint():
-    #     pass
-    # def lowerRightPoint(self)->Point:
-    #     pass
-    # def upperLeftPoint(self)->Point:
-    #     pass
-    # def upperRightPoint(self)->Point:
-    #    pass  
+    __y2:int  
     def isOverlapping(self,rect:object,padding=0)->bool:
         pass
     def update(self,rect:object)->None:
         pass
     def overlapUpdate(self,rect:object)->bool:
         pass
+
+class classObj():
+    pass
+
