173bb7f850d61c8b165d3438f3106635622e8e4d
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-18 17:26:09 +0545

    Update num.py

--- num.py
+++ num.py
@@ -1,9 +1,9 @@
 class Num():
     def __init__(self,num:int) -> None:
-        self.num=num
-        pass
+        self.__num=num
+    def __call__(self) -> int:
+        return self.__num
     def __str__(self) -> str:
         return self.num.__str__()
     def __repr__(self) -> str:
         return self.num.__repr__()
-        pass
