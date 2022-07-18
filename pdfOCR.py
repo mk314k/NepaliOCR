823aa27cf53448a4960fa378f6fa3428bc7b4e92
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-18 17:24:21 +0545

    Update pdfOCR.py

--- pdfOCR.py
+++ pdfOCR.py
@@ -1,40 +1,29 @@
 import numpy as np
 import cv2
-import os
-from pdfClass import superPDF
+from pdfClass import PDFReader, PDFWriter
 from detector import detect
 from recognizer import recognize
 
-filepath = os.getcwd()
-
 def main(fileName):
-    pdfIn = superPDF(fileName, mode='r')
+    pdfIn = PDFReader(fileName, mode='r')
     imgNames=pdfIn.toImage()
-    pdfOut = superPDF(fileName[:-4]+'-out.pdf',mode='w')
+    pdfOut = PDFWriter(fileName[:-4]+'-out.pdf',mode='w')
+
     for imgName in imgNames:
         pdfOut.newPage()
         img = cv2.imread(imgName)
         extractedData = detect(img)
-        for imgData in extractedData.texts():
-            imgBlock = extractedData.getImage(imgData)
-            textBlock = recognize(imgBlock)
-            pdfOut.addText(textBlock,imgData)
 
-        for imgData in extractedData.images():
-            img=extractedData.getImage(imgData)
-            pdfOut.addImage(img,imgData)
-
-    pdfOut.close()
-    pdfIn.close()
-
-            
-
-
-
-
-
+        for dataRect in extractedData:
+            if dataRect.type == 'text':
+                recognizedText = recognize(dataRect.getImage(img))
+                pdfOut.addText(dataRect,recognizedText)
+            elif dataRect.type == 'Image':
+                pdfOut.addImage(dataRect)
+            elif dataRect.type == 'Table':
+                pdfOut.addTable(dataRect)
     pass
 
 if __name__=='__main__':
-    #TODO UI to get filename
-    main(filepath+"")
+    filename = '' #TODO UI to get filename
+    main(filename)
