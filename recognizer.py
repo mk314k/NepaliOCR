88f5daa12eac9e1226f8285b2ef3a6bc8697894d
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-18 17:23:30 +0545

    initiated recognition module

--- recognizer.py
+++ recognizer.py
@@ -1,8 +1,22 @@
-from detector import detector,FILEPATH,FILEPREFIX
 import cv2
 import numpy as np
+from utility import CRNNModelPath
+import tensorflow as tf2
+from learn import recognitionModel
 
-def recognizer(filename:str):
-    detected = detector(filename)
-    detected["file"]=FILEPATH+FILEPREFIX+filename
-    return detected
+def preProcess(img:np.ndarray):
+
+    pass
+
+def postProcess(pred):
+
+    pass
+
+
+def recognize(img:np.ndarray):
+    imgp = preProcess(img)
+    recogModel = recognitionModel(training=False)
+    recogModel.load_weights(CRNNModelPath)
+    pred = recogModel.predict(imgp)
+
+    return postProcess(pred)
