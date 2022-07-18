47ed2a09f4a7a325a3b5d1fa53d8e42ce9801494
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-18 17:17:41 +0545

    documentation begins

--- RectSet.py
+++ RectSet.py
@@ -2,50 +2,106 @@ from Rect import Rect
 import numpy as np
 from DynamicNP import DynamicNpy
 from num import Num
+import math
 
 class RectSet():
-    def __init__(self,dimension:int|tuple,blockSize=200,blockMode=True) -> None:
+    """This class represents a set of rectangles, specifically designed to check and avoid 
+        overlapping if necessary.
+    """
+    def __init__(self, dimension:int|tuple, blockSize=200, blockMode=True, source=None) -> None:
+        """AI is creating summary for __init__
+
+        Args:
+            dimension (int|tuple): [description]
+            blockSize (int, optional): [description]. Defaults to 200.
+            blockMode (bool, optional): [description]. Defaults to True.
+            source (str|np.ndarray, optional): [description]. Defaults to None.
+        """
         self.__blocksize = blockSize
         self.__blockMode = blockMode
-        self.image = DynamicNpy(Rect)
+        self.__source = source
+        self.__image = DynamicNpy(Rect)
         if isinstance(dimension,int):
             dimension =(dimension,dimension)
         self.__dimension = dimension
         if blockMode:
-            dimension = (dimension[0]//blockSize+1,dimension[1]//blockSize+1)
-            self.indexArray = np.array([None]*dimension[0]*dimension[1]).reshape(dimension).astype(DynamicNpy)
-    def __eq__(self, o: object) -> bool:
-        return self.image.__eq__(o.image)
+            dimension = (math.ceil(dimension[0]/blockSize),math.ceil(dimension[1]/blockSize))
+            self.__indexArray = np.array([None]*dimension[0]*dimension[1]).reshape(dimension).astype(DynamicNpy)
 
-    def addRect (self,rect:Rect):
+    def __eq__(self, o: object) -> bool:
+        """AI is creating summary for __eq__
+
+        Args:
+            o (object): [description]
+
+        Returns:
+            bool: [description]
+        """
+        return self.__image.__eq__(o.__image)
+
+    def addRect (self,rect:Rect,padding=0):
+        """AI is creating summary for addRect
+
+        Args:
+            rect (Rect): [description]
+            padding (int, optional): [description]. Defaults to 0.
+        """
+        def updateRect(blocks):
+            """AI is creating summary for updateRect
+
+            Args:
+                blocks ([type]): [description]
+
+            Returns:
+                [type]: [description]
+            """
+            for block in blocks:
+                if self.__indexArray[block]==None:
+                    self.__indexArray[block]=DynamicNpy(Num)
+                else:
+                    for i, index in enumerate(self.__indexArray[block]):
+                        if self.__image[index()] and self.__image[index()].type!='Table' and rect.overlapUpdate(self.__image[index()],padding):
+                            self.__image.removeIndex(index())
+                            self.__indexArray[block].removeIndex(i)
+                            blocks = rect.getblocks(self.__blocksize)
+                            blocks = updateRect(blocks)
+                            return blocks
+            return blocks
+
         if self.__blockMode:
             blocks = rect.getblocks(self.__blocksize)
-            for block in blocks:
-                if self.indexArray[block]==None:
-                    self.indexArray[block]=DynamicNpy(Num)
-                else:
-                    for i, index in enumerate(self.indexArray[block]):
-                        if rect.overlapUpdate(self.image[index.num]):
-                            self.image.removeIndex(index.num)
-                            self.indexArray[block].removeIndex(i)
-            index = self.image.append(rect)
+            if rect.type != "Table":
+                #TODO
+                blocks = updateRect(blocks)
+                # for block in blocks:
+                #     if self.__indexArray[block]==None:
+                #         self.__indexArray[block]=DynamicNpy(Num)
+                #     else:
+                #         for i, index in enumerate(self.__indexArray[block]):
+                #             if self.__image[index.num] and self.__image[index.num].type!='Table' and rect.overlapUpdate(self.__image[index.num],padding):
+                #                 self.__image.removeIndex(index.num)
+                #                 self.__indexArray[block].removeIndex(i)
+            index = self.__image.append(rect)
             index = Num(index)
             for block in blocks:
-                self.indexArray[block].append(index)
+                if self.__indexArray[block]==None:
+                    self.__indexArray[block]=DynamicNpy(Num)
+                self.__indexArray[block].append(index)
 
         #else:
             #TODO
     def __iter__(self):
-        yield from self.image
+        """AI is creating summary for __iter__
+
+        Yields:
+            [type]: [description]
+        """
+        yield from self.__image
 
     def __repr__(self) -> str:
-        return self.image.__repr__()
-        pass
+        """AI is creating summary for __repr__
 
-
-
-
-
-
-
-
+        Returns:
+            str: [description]
+        """
+        return self.__image.__repr__()
