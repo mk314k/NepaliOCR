d500a55330eb06bacb79cebfc7b50ba4f0c188d7
mk314k <87839505+mk314k@users.noreply.github.com>
2022-07-18 17:25:18 +0545

    completed pdfreader class

--- pdfClass.py
+++ pdfClass.py
@@ -10,53 +10,58 @@ import locale
 class PDFReader():
     def __init__(self,fileName:str) -> None:
         self.__fileName=fileName
+        self.__pdf = PdfReader(fileName)
     
-    def toImage(self, outExt='jpg'):
+    def toImage(self, outExt='jpg',resolution = 300):
         if outExt=='jpg':
-            self.toJPG()
+            return self.toJPG(resolution)
 
-    def toJPG(self):
+    def toJPG(self, resolution=300):
+        fileNameOnly = self.__fileName[:-4]
         args = ["pef2jpeg", # actual value doesn't matter
             "-dNOPAUSE",
             "-sDEVICE=jpeg",
-            "-r144",
-            "-sOutputFile=" + self.__fileName[:-4]+'%02d.jpg',
+            f"-r{resolution}",
+            "-sOutputFile=" +fileNameOnly+'%d.jpg',
             self.__fileName]
 
         encoding = locale.getpreferredencoding()
         args = [a.encode(encoding) for a in args]
-
         ghostscript.Ghostscript(*args)
+
+        return [f'{fileNameOnly}{i}.jpg' for i in range(self.__pdf.getNumPages())]
 
     def extractText(self,pages:int|list|range=-1):
         pdf = PdfReader(self.__fileName)
         if pages==-1:pages=range(pdf.getNumPages())
+        page_data =''
         for page_count in pages:
             page = pdf.getPage(page_count)
-            page_data = page.extractText()
+            page_data += page.extractText()
         return page_data
 
 
 class PDFWriter():
     def __init__(self,filename) -> None:
-        self.__pdf =FPDF(orientation='P',unit='mm', format='A4')
+        self.__pdfData= None
+        #self.__pdf =FPDF(orientation='P',unit='mm', format='A4')
         self.__filename = filename
     
-    def addText(self,textData):
-        self.__pdf.set_font_size(textData['size'])
-        self.__pdf.set_text_color(*textData['colr'])
-        self.__pdf.cell(textData['width'],textData['height'],textData['text'])
+    # def addText(self,textData):
+    #     self.__pdf.set_font_size(textData['size'])
+    #     self.__pdf.set_text_color(*textData['colr'])
+    #     self.__pdf.cell(textData['width'],textData['height'],textData['text'])
 
-    def addImage(self,imageData):
-        img=Image.fromarray(imageData['img'])
-        self.__pdf.image(img,x=imageData['x'],y=imageData['y'],w=imageData['w'],h=imageData['h'])
+    # def addImage(self,imageData):
+    #     img=Image.fromarray(imageData['img'])
+    #     self.__pdf.image(img,x=imageData['x'],y=imageData['y'],w=imageData['w'],h=imageData['h'])
 
-    def addTable(self,tableData):
+    # def addTable(self,tableData):
 
-        pass
+    #     pass
     
-    def close(self):
-        self.__pdf.output(self.__filename)
+    # def close(self):
+    #     self.__pdf.output(self.__filename)
 
 def superPDF(fileName:str,mode='r')->object:
     if mode =='r':
